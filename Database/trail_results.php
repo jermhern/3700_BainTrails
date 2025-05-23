52742cb5e98bfdf5d08f34d3dc5856b23ca94464
Jeremy Hernandez <jermhern@gmail.com>
2025-05-23 14:59:14 -0700

    Fixed file pathing & issues with how queries were handled on pages

--- Database/trail_results.php
+++ Database/trail_results.php
@@ -4,7 +4,7 @@
     <script src='https://api.mapbox.com/mapbox-gl-js/v3.11.0/mapbox-gl.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v3.11.0/mapbox-gl.css' rel='stylesheet' />
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <script src="script.js"></script>
     <title>Results</title>
   </head>
@@ -13,13 +13,15 @@
     <header>
       <!-- the entire top section of the page -->
       <div class="header_parent">
-        <a class="logo" href="./index.html"><h2>BainTrails [Logo]</h2></a>
-        <form action="trail_results.php">
-          <input method="POST" type="text" id="nav_search" name="search" placeholder="search">
+        <a class="logo" href="../index.html">
+          <img src="../assets/baintrail_logo.png" alt="BainTrails Logo">
+        </a>
+        <form method="POST" action="trail_results.php">
+          <input type="text" id="nav_search" name="search" placeholder="search">
         </form>
         <!-- Links on right side of header -->
         <ul class="nav">
-          <a href="./results.html">Trails</a>
+          <a href="../results.html">Trails</a>
           <a href="lost_page.html">Account</a>
         </ul>
       </div>
@@ -34,11 +36,11 @@
 
         <!-- Insert some sort of for loop to populate the data from the DB here  -->
         <?php
-          echo '<h2>Searched for: ' . $_POST['search'] . '</h2>';
+          echo '<h1>Searched for: ' . $_POST["search"] . '</h1>';
         ?>
 
         <div class="trail">
-          <img class="trail_img" src="assets/trail_placeholder.png" alt="trail icon">
+          <img class="trail_img" src="../assets/trail_placeholder.png" alt="trail icon">
 
           <div class="trail_info">
             <div class="name_favbutton">
@@ -46,7 +48,7 @@
               <button class="fav_button">Heart Logo</button>
             </div>
             
-            <p>Distance</p>
+            <p>$Distance</p>
             <div class="info_reviews">
               <p class="features">$Features</p>
               <p class="reviews">$Reviews</p>
@@ -61,7 +63,7 @@
 					mapboxgl.accessToken = 'pk.eyJ1IjoiamhlcjEwIiwiYSI6ImNtOXhjZHV0dTE2ZHMyam9oYm9vcDJ2ZDcifQ.blzKgRcRdMwQCkUYE3AeeQ';
 					const map = new mapboxgl.Map({
 						container: 'map', // container ID
-						style: 'mapbox://styles/mapbox/standard', // style URL
+						style: 'mapbox://styles/jher10/cmasqzz9a01gq01sie7d6bs8s', // style URL
 						center: [-122.5383, 47.6454], // starting position [lng, lat]
 						// [-122.5383, 47.6454] Bainbridge island for baintrails
 						zoom: 11, // starting zoom
