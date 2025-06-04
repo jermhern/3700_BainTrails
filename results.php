81ab71f4e7c7e5b8b24be39a075e096f59fa0c31
Jeremy Hernandez <jermhern@gmail.com>
2025-06-04 10:43:50 -0700

    Added ad-hoc query with trail data

--- results.php
+++ results.php
@@ -1,10 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
     <!-- Map -->
     <script src='https://api.mapbox.com/mapbox-gl-js/v3.11.0/mapbox-gl.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v3.11.0/mapbox-gl.css' rel='stylesheet' />
 
-    <link rel="stylesheet" href="../style.css">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
     <title>Results</title>
 
@@ -16,23 +18,23 @@
 
       // create the connection
       $conn = mysqli_connect($servername, $user, $pass, $dbname);
-      ?>
+    ?>
   </head>
 
   <body>
     <header>
       <!-- the entire top section of the page -->
       <div class="header_parent">
-        <a class="logo" href="../index.html">
-          <img src="../assets/baintrail_logo.png" alt="BainTrails Logo">
+        <a class="logo" href="./index.html">
+          <img src="assets/baintrail_logo.png" alt="BainTrails Logo">
         </a>
-        <form method="POST" action="trail_results.php">
+        <form method="POST" action="./database/trail_results.php">
           <input type="text" id="nav_search" name="search" placeholder="search">
         </form>
         <!-- Links on right side of header -->
         <ul class="nav">
-          <a href="../results.php">Trails</a>
-          <a href="../account.html">Account</a>
+          <a href="./results.php">Trails</a>
+          <a href="account.html">Account</a>
         </ul>
       </div>
     </header>
@@ -42,27 +44,26 @@
       <div class="results">
         <div class="filters">
           <h2>Filters</h2>
-          <form method="POST" action="trail_results.php">
+          <form method="POST" action="./database/trail_results.php">
             <input type="checkbox" id="kid" name="kid_filter" value="kids">
-            <label for="kid_filter">Good for kids</label><br>
+            <label for="kid">Good for kids</label><br>
             <input type="checkbox" id="dog" name="dog_filter" value="dogs">
-            <label for="dog_filter">Good for dogs</label><br>
+            <label for="dog">Good for dogs</label><br>
             <input type="checkbox" id="easy" name="easy_filter" value="easy">
-            <label for="easy_filter">Easy</label><br>
+            <label for="easy">Easy</label><br>
             <input type="checkbox" id="moderate" name="moderate_filter" value="moderate">
-            <label for="moderate_filter">Moderate</label><br>
+            <label for="moderate">Moderate</label><br>
             <input type="submit" value="Apply">
           </form>
         </div>
 
-        <!-- Insert some sort of for loop to populate the data from the DB here  -->
         <?php
             if (!$conn) {
               die('<h2 style="color: green;">connection failed: ' . mysqli_connect_error() . '</h2>');
             } 
 
             // get the query
-            $sql = 'select * from TRAIL natural join Photos where Trail_name like "%'. $_POST["search"] .'%"';
+            $sql = 'select * from TRAIL natural join Photos';
 
             // // pass the query to sql 
             $result = mysqli_query($conn, $sql);
@@ -137,15 +138,17 @@
 						style: 'mapbox://styles/jher10/cmasqzz9a01gq01sie7d6bs8s', // style URL
 						center: [-122.5383, 47.6454], // starting position [lng, lat]
 						// [-122.5383, 47.6454] Bainbridge island for baintrails
-						zoom: 11, // starting zoom
+						zoom: 12, // starting zoom
 					});
 
-          // Display the data points of 'trails'
+          // Display the data points of 'caretakers'
 					map.on('load', () => {
 					    map.addSource('Bainbridge-Parks', {
 					      type: 'vector',
 					      url: 'mapbox://jher10.cm9xch21p12ln1opbkv1cz00x-8ckm1' // my tileset ID
 					    });
+              
+              // Show the data placed into map from mapbox
 					    map.addLayer({
 					      'id': 'Bainbridge-Parks',
 					      'type': 'circle', 
@@ -164,16 +167,18 @@
     <!-- the bottom of the page -->
     <footer>
       <div class="footer_parent">
-        <p id="1"><a href="../lost_page.html">About</a></p>
-        <p id="2"><a href="../lost_page.html">Contact</a></p>
+        <p id="1"><a href="lost_page.html">About</a></p>
+        <p id="2"><a href="lost_page.html">Contact</a></p>
       </div>
+
+      <?php 
+        // disconnect
+        mysqli_free_result($result);
+        mysqli_close($conn);
+      ?>
+      <script src="script.js"></script>
     </footer>
 
-    <script src="script.js"></script>
-    <?php 
-      // disconnect
-      mysqli_free_result($result);
-      mysqli_close($conn);
-    ?>
+    
   </body>
 </html>
